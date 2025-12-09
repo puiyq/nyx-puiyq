2aa74ffcff8486173a049feed2b03db54ba22a1a
loner <2788892716@qq.com>
2025-12-10 00:28:18 +0800

    granix.yaml

--- flake.nix
+++ flake.nix
@@ -95,10 +95,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org/"
       "https://chaotic-nyx.cachix.org/"
+      "https://cache.garnix.io"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     ];
   };
 }
