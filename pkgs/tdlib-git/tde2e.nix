45b318ff624b8473325d99636f0eeed0e3ceec5a
Pui Yong Qing <puiyongqing@gmail.com>
2025-12-18 02:20:53 +0800

    drop all unused packages and module

--- pkgs/tdlib-git/tde2e.nix
+++
@@ -1,8 +0,0 @@
-{ final, ... }:
-
-final.tdlib_git.overrideAttrs (prevAttrs: {
-  pname = "tde2e";
-  cmakeFlags = (prevAttrs.cmakeFlags or [ ]) ++ [
-    (final.lib.cmakeBool "TD_E2E_ONLY" true)
-  ];
-})
