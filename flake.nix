44bdb484aff465970a20b41cbd41d7e095a17143
Pui Yong Qing <puiyongqing@gmail.com>
2026-01-13 22:10:39 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
   inputs = {
     # For all users
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
